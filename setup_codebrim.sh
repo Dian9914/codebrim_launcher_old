47ee6f0de855de0d75a6bcad39de656c68505beb
dian9914 <edusotelo1999@gmail.com>
2022-02-02 23:48:22 +0100

    added remove orders to clean up

--- setup_codebrim.sh
+++ setup_codebrim.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-while read url; do
-    wget $url -O ./codebrim.zip
-done < urls.txt
+wget -i urls.txt $url -O ./codebrim.zip
 
 jar xvf codebrim.zip
 
 python ./codebrim2coco.py ./original_dataset/ -o ./codebrim_coco/
+
+rm ./codebrim.zip
+rm -r ./original_dataset 
+rm -r ./__MACOSX
